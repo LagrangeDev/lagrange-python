0bd9fa8d5b25dfca357829b2b513560e963cb84a
Linwenxuan <llwwxxllwwxx@gmail.com>
2024-02-17 12:39:04 +0800

    fixed parser for sso

--- lagrange/client/sso.py
+++ lagrange/client/sso.py
@@ -3,6 +3,7 @@ import zlib
 from io import BytesIO
 from typing import Tuple, Union
 
+from lagrange.utils.binary.reader import Reader
 from lagrange.utils.crypto.tea import qqtea_decrypt
 
 
@@ -32,24 +33,19 @@ def parse_sso_header(raw: bytes, d2_key: bytes) -> Tuple[int, str, bytes]:
 def parse_sso_frame(
         buffer: bytes
 ) -> Union[
-    Tuple[int, int, str, str, bytes, bytes],
-    Tuple[int, int, str]
+    Tuple[int, int, str, bytes, bytes],
+    Tuple[int, int]
 ]:
-    buf = BytesIO(buffer)
-    head_len, seq, ret_code = struct.unpack("!Iii", buf.read(12))
-
-    buf_len = len(buffer) - 8
-    if head_len != buf_len:
-        print(f"WARN: length of buffer does not match({head_len} vs {buf_len})")
-
-    extra = parse_lv(buf).decode()
+    reader = Reader(buffer)
+    head_len, seq, ret_code, session_id = reader.read_struct("!I3i")
     if ret_code != 0:
-        return seq, ret_code, extra
-    command_name = parse_lv(buf).decode()
-    session_id = parse_lv(buf)
-    compress_type = struct.unpack('>I', buf.read(4))[0]
+        return seq, ret_code
+    cmd = reader.read_string_with_length("u32")
+    reader.read_string_with_length("u32")
+    compress_type = reader.read_u32()
+    reader.read_bytes_with_length("u32", False)
 
-    data = parse_lv(buf)
+    data = reader.read_bytes_with_length("u32", False)
     if data:
         if compress_type == 0:
             pass
@@ -60,6 +56,4 @@ def parse_sso_frame(
         else:
             raise TypeError(f"Unsupported compress type {compress_type}")
 
-    #print(head_len, seq, ret_code, extra, compress_type, data, buf.read())
-
-    return seq, ret_code, extra, command_name, session_id, data
+    return seq, ret_code, cmd, session_id, data
