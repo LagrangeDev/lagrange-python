d1306c332cb4d13825ae4b8018016965c7f99721
wyapx <admin@nullcat.cn>
2024-08-18 05:08:17 +0800

    fix(event): GroupNudge error when recv VIP nudge

--- lagrange/client/server_push/msg.py
+++ lagrange/client/server_push/msg.py
@@ -105,8 +105,10 @@ async def msg_push_handler(client: "Client", sso: SSOPacket):
                         grp_id,
                         attrs["uin_str1"],
                         attrs["uin_str2"],
-                        attrs.get("action_str", ""),  # ?
-                        attrs.get("suffix_str", ""),
+                        attrs["action_str"]
+                        if "action_str" in attrs
+                        else attrs["alt_str1"],  # ?
+                        attrs["suffix_str"],
                         attrs,
                         pb.body.attrs_xml,
                     )
