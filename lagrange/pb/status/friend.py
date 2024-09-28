109293360077b75e9d5a86bc8e8577f1db0713d8
HornCopper <68726147+HornCopper@users.noreply.github.com>
2024-09-29 05:16:50 +0800

    Update lagrange/pb/status/friend.py
    Co-authored-by: nullcat <a1025952421@outlook.com>

--- lagrange/pb/status/friend.py
+++ lagrange/pb/status/friend.py
@@ -17,7 +17,7 @@ class PBFriendRecall(ProtoStruct):
 class FriendRequestInfo(ProtoStruct):
     to_uid: str = proto_field(1)
     from_uid: str = proto_field(2)
-    verify: str = proto_field(10) # 验证消息：我是...
+    verify: str = proto_field(10)  # 验证消息：我是...
     source: str = proto_field(11)
 
 class PBFriendRequest(ProtoStruct):
