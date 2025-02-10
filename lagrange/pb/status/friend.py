8f7ea651dab41d65157cc6161f2c6eb83c59e7cc
Decrabbit <decrabbit@163.com>
2025-02-10 16:49:40 +0800

    fuck tx, resolve friendquest wrong field

--- lagrange/pb/status/friend.py
+++ lagrange/pb/status/friend.py
@@ -17,6 +17,7 @@ class PBFriendRecall(ProtoStruct):
 class FriendRequestInfo(ProtoStruct):
     to_uid: str = proto_field(1)
     from_uid: str = proto_field(2)
+    source_new :str = proto_field(5)
     verify: str = proto_field(10)  # 验证消息：我是...
     source: str = proto_field(11)
 
